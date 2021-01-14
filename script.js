8ff5cd5890a4444c9fd8089414dbcc269efbcbb1
Gabriel Cardoso <x399939@gruposantander.com>
2021-01-14 00:52:50 -0300

    updating script

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const searchBySearchQuery = () => {
-    let query = document.querySelector('#searchQuery').value;
+    let { value: query } = document.querySelector('#searchQuery');
     window.open(`https://www.youtube.com/results?search_query=${query}`);
 };
 
-const onKeypressEvent = (e) => {
-    if (e.key === 'Enter') searchBySearchQuery();
+const onKeypressEvent = ({ key }) => {
+    if (key === 'Enter') searchBySearchQuery();
 };
 
-onClickEventListenerStart = () => {
+startOnClickEventListener = () => {
     document
         .querySelector('#btnSearch')
         .addEventListener('click', searchBySearchQuery);
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-    onClickEventListenerStart();
+    startOnClickEventListener();
     document.addEventListener('keypress', onKeypressEvent);
 });
